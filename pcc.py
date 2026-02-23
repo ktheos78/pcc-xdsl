01e6476efb62f14d6ca278ac6b9b9b5ee2cd4605
theos78 <theosguitar@gmail.com>
2026-02-24 01:24:23 +0200

    add wrapper program to compile through CLI

---
+++ pcc.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+import sys
+from xdsl.transforms.common_subexpression_elimination import cse
+from xdsl.transforms.dead_code_elimination import dce
+
+from src.frontend_ast import *
+from src.frontend_mlir_gen import *
+from src.backend_optimization import *
+from src.backend_arm_dialect import *
+from src.backend_printer import *
+
+if len(sys.argv) < 2:
+    sys.exit("Error: no filename provided. Usage: ./pcc.py [--emit-all] <filename>")
+
+emit_all = False
+if (sys.argv[1] == "--emit-all"):
+    emit_all = True
+    in_file = sys.argv[2]
+
+else:
+    in_file = sys.argv[1]
+
+# generate AST
+p = Parser()
+ast = p.walk(in_file)
+
+# generate high-level MLIR from AST 
+gen = MLIRGenerator()
+module = gen.compile(ast)
+if (emit_all):
+    filename = f"{in_file[:len(in_file) - 2]}.mlir"
+    f = open(filename, "w+")
+    print(module, file=f)
+
+# apply optimizations
+apply_all_optimizations(module)   # canonicalizations
+cse(module)                       # common subexpression elimination
+dce(module)                       # dead code elimination
+if (emit_all):
+    filename = f"{in_file[:len(in_file) - 2]}-optimized.mlir"
+    f = open(filename, "w+")
+    print(module, file=f)
+
+# lower MLIR to ARM dialect
+lower(module)
+if (emit_all):
+    filename = f"{in_file[:len(in_file) - 2]}-arm.mlir"
+    f = open(filename, "w+")
+    print(module, file=f)
+
+# print assembly
+filename = f"{in_file[:len(in_file) - 2]}.s"
+f = open(filename, "w+")
+print_asm(module, out_file=f)
